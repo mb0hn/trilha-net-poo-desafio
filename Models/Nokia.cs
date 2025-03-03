4717e54ba14b929a1488eb1acfdccb0fa5b8242b
Marcelo Bohn da Silva <bohn.marcelo@gmail.com>
2025-03-02 22:00:50 -0300

    implementado

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,20 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    // IMPLEMENTADO
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero)
+        {
+            Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+        }
+
+        // IMPLEMENTADO
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no Nokia");
+        }
     }
 }
--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,23 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        // IMPLEMENTADO
+        protected string Modelo { get; set; }
+        protected string IMEI { get; set; }
+        protected int Memoria { get; set; }
+
+        public Smartphone(string numero,string modelo, string imei, int memoria)
+        {
+            Numero = numero;
+            // IMPLEMENTADO
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+        }
 
         public Smartphone(string numero)
         {
-            Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+
         }
 
         public void Ligar()
